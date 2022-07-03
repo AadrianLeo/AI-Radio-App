5286365b318a453f8e3c26b8aca820d10b781f1a
thecoderpunk <ravisingh02082001@gmail.com>
2022-07-03 18:49:07 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:ai_radio/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AI Radio',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/utils/ai_util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static Color PrimaryColor1 = Vx.orange400;
+  static Color PrimaryColor2 = Vx.purple500;
+}
